8775c85186bae2e95191eecf4e5ba6dd132086e3
patrickleomackey-is <plmackey@my.niacc.edu>
2020-11-11 20:43:16 -0600

    the one thing in my project I was doing wrong was using var and let, turns out this was killing me. I am so happy this thing works. My new goal is to keep a running tally of wins losses ties etc...

--- RockPaperScissors/public_html/RockPaperScissors.js
+++ RockPaperScissors/public_html/RockPaperScissors.js
@@ -1,15 +1,60 @@
-function generateComputerChoice() {
-//generate a random number between 1 and 3
-//1 = rock, 2 = paper, 3 = scissors
-//the function should return the name of the computer's choice
+ losses =[];ties =[]; wins=[];
+function cpThrow(){ 
+    
+    
+    Random = Number(Math.floor(Math.random()*3));
+    
+    if (Random === 0){
+         CPU = "Rock";
+        document.write("It threw a " + CPU);
+    } else if (Random === 1){
+         CPU = "Paper";
+        document.write("It threw a  "+ CPU);}
+    else if(Random === 2){
+         CPU = "Scissors";
+        document.write("It threw a "+ CPU);}
+    return CPU;
+    }
+    
+    document.addEventListener('submit',cpThrow()).write(CPU);
+    
 
-}
+
+
+
+
 
 function PlayTheGame() {
 //declare variables to hold the player choice and the computer choice
 
+var userThrow = document.getElementsByName("choice");
 
+for (i =0;i < userThrow.length; i++){
+    if (userThrow[i].checked === true){
+     urChoice = userThrow[i].value;   }}
+     
 
+if (urChoice === CPU){alert ("tie "+CPU+" was thrown by your opponent!");ties++;}
+
+else if (urChoice ==="Rock" && CPU ==="Paper"){alert(CPU+ " covers "+urChoice+ " you lost\n\
+this time");++losses;}
+    
+else if (urChoice ==="Rock" && CPU ==="Scissors"){alert("Your "+urChoice+" smashes \n\
+the opponent's "+CPU);++wins;} 
+    
+else if (urChoice ==="Paper"&& CPU ==="Scissors"){alert("The opponent's "+CPU+" shreds\n\
+your "+urChoice);++losses;}
+    
+else if (urChoice ==="Paper"&& CPU ==="Rock"){alert("Your "+urChoice+ " envelopes \n\
+the opponent's "+CPU);++wins;}
+
+else if (urChoice ==="Scissors"&& CPU ==="Rock"){alert("Your "+urChoice+" was \n\
+crushed by the opponent's "+CPU);++losses;}
+    
+else if (urChoice ==="Scissors"&& CPU ==="Paper"){alert("Your "+urChoice+" chops up \n\
+your opponent's "+CPU);++wins;}    
+    
+    document.write("<br>W's "+wins+"<br>Ties "+ties+"<br>L's "+losses);
 //use getElementsByName to get the form radio button values
 //loop through the array to find which one is checked
 //store the checked radio button's value in the variable for player choice
